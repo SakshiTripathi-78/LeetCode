4417979f645f86806a817e621219f313da1ba596
Sakshi Ashok Tripathi <sakshitripathi117@gmail.com>
2026-06-25 11:17:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (54.19%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long reverse=0;
+        int digit;
+        while(x!=0){
+            digit=x%10;
+            
+            reverse=reverse*10+digit; 
+            x=x/10;
+
+            if (reverse>INT_MAX || reverse<INT_MIN){
+                return 0;
+            }
+        }
+        return reverse;
+        
+    }
+};
